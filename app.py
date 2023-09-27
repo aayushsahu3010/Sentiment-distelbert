0bb7d1b3931e4d68d0aded07561a4bb26a48a350
JacobJ215 <jacob.johnson2718@gmail.com>
2023-09-27 15:26:36 -0500

    created streamlit frontend

---
+++ app.py
@@ -0,0 +1,30 @@
+import string
+import re
+from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
+import numpy as np
+import streamlit as st
+
+# Load the fine-tuned DistilBERT model
+model_name = 'distilbert-base-uncased'
+tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+model = TFDistilBertForSequenceClassification.from_pretrained('./models/fine_tuned_distilbert', num_labels=2)
+
+# Function to perform sentiment analysis
+def perform_sentiment_analysis(text):
+    inputs = tokenizer(text, padding='max_length', truncation=True, max_length=128, return_tensors='tf')
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_label = np.argmax(logits).item()
+    sentiment = 'Positive' if predicted_label == 1 else 'Negative'
+    return sentiment
+
+st.title("Sentiment Analysis with DistilBERT")
+
+review_text = st.text_area("Enter a review:")
+
+if st.button("Submit"):
+    if review_text:
+        sentiment = perform_sentiment_analysis(review_text)
+        st.write(f"Sentiment: {sentiment}")
+    else:
+        st.warning("Please enter a review.")
